a23ff341db2634c3c22fc0ca3c1d8ddf5886c375
moudzx <mohamadmostafa79@gmail.com>
2026-05-11 17:22:13 +0300

    Remove registration pages; add forgot/new password pages
    Removed DoctorRegistration, PatientRegistration, and Registration pages with their code-behind and designer files. Updated project file to reflect these removals and added new ForgotPassword and NewPassword pages with basic structure and code-behind.

--- Pages/Account/NewPassword.aspx.cs
+++ Pages/Account/NewPassword.aspx.cs
@@ -7,15 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace MediCare.Pages.Account
 {
-    public partial class PatientRegistration : System.Web.UI.Page
+    public partial class NewPassword : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-        protected void btnPatientSignUp_Click(object sender, EventArgs e)
-        {
-            // Patient signup logic later
         }
     }
 }
